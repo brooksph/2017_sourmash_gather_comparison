e7c0684aad2390c63c17a945f4b95726201be115
brooksph <philliptbrooks@gmail.com>
2017-05-30 04:53:36 -0400

    updated gather output

--- scripts/classify.sh
+++ scripts/classify.sh
@@ -4,5 +4,5 @@
 #comparing signatures calculated with sourmash to a sequence bloom tree containing 
 #sequences from respresenting the entirety of the NCBI Refseq and genbank databases 
  
-~/sourmash/sourmash gather SRR606249.pe.qc.fq.abundtrim.1k.sig ../sbt/refseq-k31.sbt.json ../sbt/genbank-k31.sbt.json -o ../results/${i}refseq_and_genbank_gather_output_1k.csv
+~/sourmash/sourmash gather ../sigs/podar-reads.10k.sig ../sbt/refseq-k31.sbt.json ../sbt/genbank-k31.sbt.json -o ../results/refseq_and_genbank_gather_output_10k.csv
  
